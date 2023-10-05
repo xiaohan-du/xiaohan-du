ccc11a0d041a506746dce46daf3e2ccbd072ddb4
thinkvantagedu <duxiaohan2006@gmail.com>
2023-10-05 12:37:05 +0100

    Add a navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 'use client'
+import { Navbar } from './components/navbar';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Navbar />
       Navbar
       Software Engineer
       Xiaohan Du
